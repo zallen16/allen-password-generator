577123dc7457cc4c8dcdb926bc7beea2bb58b7e9
zallen16 <126983111+zallen16@users.noreply.github.com>
2023-11-15 00:17:35 -0500

    created assignment code in js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Assignment code here
+const minLength = 8;
+const maxLength = 128;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
 
 
 // Get references to the #generate element
