2ca8283e367f1f565661a65ba3f3f88830408ad7
zallen16 <126983111+zallen16@users.noreply.github.com>
2023-11-16 13:45:23 -0500

    added generatepassword function, syntax changes

--- script.js
+++ script.js
@@ -7,14 +7,25 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "@#$%^&*()_+~|}{[]></-=";
 
-const allChars = upperCase + lowerCase + number + symbol;
 
-var question1 = confirm("Is this true?");
-console.log("question1", question1);
-var question2 = prompt("Type below");
-console.log("question2", question2);
+function generatePassword () {
+  
+  var allChars = "";
+  var question1 = confirm("Is this true?");
+    console.log("question1", question1);
+  var question2 = prompt("Type anything below");
+    console.log("question2", question2);
 
-var question3 = prompt("")
+  var passwordLength = prompt("Length of password?");
+    console.log("question3", question3);  
+    if (question3) {
+        var choice = prompt;
+      }
+      if (choice == i<8 || i>128){
+        return;
+      };
+}
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
